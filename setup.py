50fc31c6f86224e137243049b0d12d32d22edc91
mduran80 <manuel.duran@gmail.com>
2021-01-11 14:50:52 -0500

    Updated package info

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='pymadoka',
     version='0.1',
     py_modules=['pymadoka'],
+    author = "Manuel Durán",
+    author_email = "manuelduran@gmail.com",
+    description = ("A library to control Daikin BRC1H (Madoka) thermostats"),
+    license = "MIT",
+    keywords = "thermostat homeautomation bluetooth",
     install_requires=[
         'click',
         'bleak',
