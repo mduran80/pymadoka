23db7dacf9f9dd5e03ef57b95f2bffbe1461360a
Manuel Durán <manuel.duran@gmail.com>
2021-01-13 02:49:15 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymadoka',
-    version='0.1',
+    version='0.1.1',
     py_modules=['pymadoka'],
     author = "Manuel Durán",
     author_email = "manuelduran@gmail.com",
